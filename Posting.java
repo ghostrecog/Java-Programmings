1614a6a7639e62013bbad3d324a3c2db65ed9e75
ghostrecog <umapathig1985@gmail.com>
2017-06-29 17:05:18 +0530

    changes
    pull

---
+++ Posting.java
@@ -0,0 +1,25 @@
+
+public class Posting {
+
+	public static void main(String [] args){
+		int[][] table = new int[5][5];
+
+
+		for(int row=0; row<table.length; row++) {
+			System.out.print(row+" ");
+			
+		for(int col=1; col<table[row].length; col++){
+			if(row!=0)
+		System.out.print(row*col+" ");
+			else
+				System.out.print(col+" ");
+			
+
+		}
+		System.out.println();
+		}
+
+
+		
+	}
+}
